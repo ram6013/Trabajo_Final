9922b3c996a01f37f71fe54c0ff33983d23a45cf
ram6013 <134057913+ram6013@users.noreply.github.com>
2023-06-14 16:45:09 +0200

    Update CardTest.java

--- CardTest.java
+++ CardTest.java
@@ -1,23 +1,16 @@
 import static org.junit.Assert.*;
-import org.junit.Before;
 import org.junit.Test;
 
 public class CardTest {
-
-    private Card card; // Objeto Card para realizar las pruebas
-
-    @Before
-    public void setUp() {
-        card = new Card("As", "Spades"); // Se crea una nueva carta antes de cada prueba
-    }
-
     @Test
     public void testGetRank() {
-        assertEquals("As", card.getRank()); // Se verifica que el valor devuelto por getRank() sea "Ace"
+        Card card = new Card("Ace", "Hearts");
+        assertEquals("Ace", card.getRank());
     }
 
     @Test
     public void testGetSuit() {
-        assertEquals("Picas", card.getSuit()); // Se verifica que el valor devuelto por getSuit() sea "Spades"
+        Card card = new Card("Ace", "Hearts");
+        assertEquals("Hearts", card.getSuit());
     }
 }
